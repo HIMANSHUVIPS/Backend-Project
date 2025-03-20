2d48ba77de166c71d66c08de1532caecf78b566c
Himanshu Sharma <sharmahimanshuvdi@gmail.com>
2025-03-20 12:31:46 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("This is the starting of my project");
+});
+
+app.listen(3000,()=>{
+    console.log("Server is Running on the Port 3000");
+});
